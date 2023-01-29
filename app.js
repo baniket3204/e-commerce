3d931c42d1fd9eaaf26c16812de2b31aa52b529c
baniket3204 <baniket3204@gmail.com>
2023-01-29 19:07:46 +0530

    added data from json files

--- app.js
+++ app.js
@@ -17,18 +17,45 @@ let cart = []
 class Products{
  async getProducts(){
     try {        
-    let result = await fetch('products.json')
-    let data = await result.json();
-    return data;
+    let result =  await fetch('products.json')
+    let data =  await result.json();
+    let products = data.items;
+    products = products.map(item => {
+        const {title , price} = item.fields;
+        const {id} = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return {title , price , id , image};
+    })
+    return products;
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
  }
 }
 
 // display products
 class UI{
-
+ displayProducts(products){
+   
+    let result = '';
+    products.forEach(product => {
+    result += 
+    `
+      <article class="product">
+        <div class="img-container">
+          <img src=${product.image} alt="image 1" class="product-img">
+          <button class="bag-btn" data-id=${product.id}>
+            <i class="fas fa-shopping-cart"></i>
+            add to cart
+          </button>
+        </div>
+        <h3>${product.title}</h3>
+        <h4>$ ${product.price}</h4>
+      </article> `
+      
+    });
+    productsDOM.innerHTML = result;
+ }
 }
 
 // local storage
@@ -40,7 +67,7 @@ document.addEventListener("DOMContentLoaded" ,()=>{
 
  const ui = new UI();
  const products = new Products();
- products.getProducts().then(data => console.log(data));
+ products.getProducts().then(products => ui.displayProducts(products));
 
 })
 
